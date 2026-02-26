148ed85c99f35efdb0dc399e2ae7fbec661ec0c1
V.Sadliy <sadliy@mail.ru>
2026-02-26 14:08:59 +0300

    vite.config.ts updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/HW_React_Props_and_Typescript/',
+  // base: '/HW_React_Props_and_Typescript/',
   plugins: [react()],
 })
